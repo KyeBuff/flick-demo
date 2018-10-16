843e97b3cf58b2ffb83934aaba13a017ce199d94
Kye Buffery <kyebuffery14@gmail.com>
2018-10-16 17:13:23 +0100

    resolved dev api url

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = 'http://flickapi2-env.sdqwkshunp.us-east-2.elasticbeanstalk.com/api/';
+export const baseURL = 'http://flick.test/api/';
 
 export default axios.create({
     baseURL: baseURL,
