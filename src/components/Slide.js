44643ae17e89a46c92a560266a937d63db241355
Kye Buffery <kyebuffery14@gmail.com>
2018-10-16 17:15:56 +0100

    removed netflix img

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import netflixImg from '../assets/img/netflix.png';
-
 export default ({media}) => (
 	<div className="slide">
 		<p className="title">{media.title}</p>
